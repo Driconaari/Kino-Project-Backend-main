363c4f4ee4886a9abeadc35a24aa94864c5d13cf
SarahKiil <sami0001@stud.kea.dk>
2024-10-02 10:57:14 +0200

    Initial commit

---
+++ src/main/java/kinoproject/security/entity/Role.java
@@ -0,0 +1,11 @@
+package kinoproject.security.entity;
+
+
+public enum Role {
+    USER,
+    ADMIN;
+
+    public static Role fromString(String roleString) {
+        return Role.valueOf(roleString.trim().toUpperCase());
+    }
+}
